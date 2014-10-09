397c70fa630699064348f200242a5e686cbf7637
ybinstock <ybinstock@gmail.com>
2014-10-08 21:01:23 -0700

    first version

--- spec/test.js
+++ spec/test.js
@@ -4,92 +4,105 @@
 var Car = require('../src/car.js'),
 expect = require('chai').expect;
 
-describe('Car', function(){
+describe('myCar', function(){
 
+var myCar;
   beforeEach(function(){
     // create a new myCar object each time
+
+myCar = new Car("mercedez", "c_class", "silver");
+
   });
 
   describe('#year', function(){
     //think about using the new Date() and getFullYear functions
     it('should be the current year', function(){
-
+      expect(myCar.year).to.equal(2014);
     });
   });
 
   describe('#state', function(){
     it('should initially be off', function(){
-
+      expect(myCar.state).to.equal("off");
     });
   });
 
   describe('#previousOwners', function(){
     it('should initially be empty', function(){
-
+      expect(myCar.previous_owners).to.be.empty;
     });
   });
 
   describe('#curretOwner', function(){
     it('should initially be manufacturer', function(){
-
+      expect(myCar.current_owner).to.equal("manufacturer");
     });
   });
 
   describe('#passengers', function(){
     it('should initially be empty', function(){
-
+      expect(myCar.passengers).to.be.empty;
     });
   });
 
   describe('#sale', function(){
 
     it('should move currentOwner to previousOwners array', function(){
-
+      myCar.sale("Yoni");
+      expect(myCar.previous_owners[0]).to.eql("manufacturer");
     });
 
     it('should update currentOwner with the new owner', function(){
-
+      myCar.sale("Yoni");
+      expect(myCar.current_owner).to.eql("Yoni");
     });
   });
 
   describe('#paint', function(){
     it('should update the color of myCar', function(){
-
+      myCar.paint("red");
+      expect(myCar.color).to.equal("red");
     });
   });
 
   describe('#start', function(){
     it('should update the state to on', function(){
-
+      myCar.start("on");
+      expect(myCar.state).to.equal("on");
     });
   });
 
   describe('#off', function(){
     it('should update the state to off', function(){
-
+      myCar.off("off");
+      expect(myCar.state).to.equal("off");
     });
   });
 
   describe('#park', function(){
     it('should make sure to only work when the car is off', function(){
-
+      myCar.park(true);
+      expect(myCar.state).to.equal("off");
     });
 
   });
 
   describe('#pickUp', function(){
     it('should add the passenger to the passengers array if car is on', function(){
-
+      myCar.pickup(true);
+      expect(myCar.passengers).to.equal("maia");
     });
 
     it('should not modify the passengers array if car is off', function(){
-
+      myCar.pickup(false);
+      expect(myCar.passengers).to.be.empty;
     });
   });
 
   describe('#dropOff', function(){
     it('should remove passenger from the passengers array if car is on', function(){
-
+      myCar.dropOff(false);
+      expect(myCar.passengers)
     });
 
     it('should leave passenger in the passengers array if car is off', function(){
--- src/car.js
+++ src/car.js
@@ -1,13 +1,63 @@
-function Car(make, model, year, color){
+function Car(make, model, color){
+
+this.make = make;
+this.model = model;
+this.year = new Date().getFullYear();
+this.previous_owners = [];
+this.current_owner = "manufacturer";
+this.color = color;
+this.state = "off";
+this.passengers = [];
 
 }
 
 Car.prototype.sale = function(newOwner){
+// make previous owner to current owner
 
+this.previous_owners.push(this.current_owner);
+this.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
+// take new color and make it the color of the car
+this.color = newColor;
+};
+Car.prototype.start = function (){
+this.state = "on";
+};
+Car.prototype.off = function () {
+this.state = "off";
+};
+
+Car.prototype.driveto = function (destination) {
+	if (this.state === "on")
+		{console.log("driving to " + destination );
+	}
+};
+
+Car.prototype.park = function () {
+	if (this.state === "off")
+	{console.log("parked!!");
+	return "parked";
+	}
+};
+
+
+Car.prototype.pickup = function () {
+	if (this.state === "on") {
+	var name = "Maia";
+	console.log ( "driving to pick up "+ name);
+	this.passengers.push(name);
+	}
+};
+
+Car.prototype.dropoff = function (name) {
+
+if (this.state === "on" && this.passengers.indexOf(name) !== -1 ) {
+	this.passengers.splice(this.passengers.indexOf(name),1) ;
+}
 
 };
+
 
 module.exports=Car;
